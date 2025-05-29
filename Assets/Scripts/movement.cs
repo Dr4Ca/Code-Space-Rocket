729a9b6a2ef08b40a564fea822089b39e64bf1b5
Dr4Ca <55535386+Dr4Ca@users.noreply.github.com>
2025-05-29 16:07:03 +0700

    Initial Commit

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,71 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class movement : MonoBehaviour
+{
+    Rigidbody rb;
+    AudioSource audioSource;
+
+    [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+    [SerializeField] float thrustForce = 10f;
+    [SerializeField] float rotationForce = 10f;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        thrust.Enable();
+        rotation.Enable();
+    }
+    void FixedUpdate()
+    {
+        processThrust();
+        processRotation();
+    }
+
+    private void processThrust()
+    {
+        if (thrust.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.up * thrustForce * Time.fixedDeltaTime, ForceMode.Acceleration);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            if (audioSource.isPlaying)
+            {
+                audioSource.Stop();
+            }
+        }
+    }
+
+    private void processRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        if (rotationInput < 0)
+        {
+            ApplyRotation(rotationForce);
+        }
+        else if (rotationInput > 0)
+        {
+            ApplyRotation(-rotationForce);
+        }
+    }
+
+    private void ApplyRotation(float rotationFrame)
+    {
+        rb.freezeRotation = true; // Prevent physics from rotating the object
+        // Rotate the object around its forward axis    
+        transform.Rotate(Vector3.forward * rotationFrame * Time.fixedDeltaTime);
+        rb.freezeRotation = false; // Re-enable physics rotation
+    }
+}
